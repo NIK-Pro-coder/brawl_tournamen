bf11605b8cfb18c2566c7ef54101386b3eb779ad
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2024-10-07 20:27:34 +0200

    First Commit

--- index.js
+++ index.js
@@ -32,27 +32,15 @@ app.get("/", function (req, res) {
 	res.send("index.html");
 });
 
-app.get("/getPlayer/:tag", function (req, res) {
-	const tag = req.params.tag;
-	runPythonScript("scripts/backend.py", ["getPlayer", tag], (err, result) => {
-		if (err) {
-			res.status(500).send(err);
-		} else {
-			res.send(JSON.parse(result));
-		}
-	});
+/*
+runPythonScript("scripts/backend.py", ["getClub", tag], (err, result) => {
+	if (err) {
+		res.status(500).send(err);
+	} else {
+		res.send(JSON.parse(result));
+	}
 });
-
-app.get("/getClub/:tag", function (req, res) {
-	const tag = req.params.tag;
-	runPythonScript("scripts/backend.py", ["getClub", tag], (err, result) => {
-		if (err) {
-			res.status(500).send(err);
-		} else {
-			res.send(JSON.parse(result));
-		}
-	});
-});
+*/
 
 const http = require("http").createServer(app);
 
